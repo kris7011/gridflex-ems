7a4278a36e37aaadc3ad141cbea7543b65059a3f
kris7011 <krisrl@hotmail.com>
2026-08-16 09:06:57 +0200

    feat: add C hardware foundation

---
+++ hardware/include/gridflex/hardware/hardware.h
@@ -0,0 +1,15 @@
+#ifndef GRIDFLEX_HARDWARE_H
+#define GRIDFLEX_HARDWARE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    const char *gridflex_hardware_name(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
