2a039b25a4f37d4746b447def4d1b394b6856d0c
Kris Riisgaard Larsen <krisrl@hotmail.com>
2026-08-16 09:14:48 +0200

    feat: add C hardware foundation (#20)

---
+++ hardware/src/hardware.c
@@ -0,0 +1,6 @@
+#include "gridflex/hardware/hardware.h"
+
+const char *gridflex_hardware_name(void)
+{
+    return "GridFlex EMS C Hardware Abstraction";
+}
---
+++ hardware/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "gridflex/hardware/hardware.h"
+
+int main(void)
+{
+    puts(gridflex_hardware_name());
+
+    return 0;
+}
